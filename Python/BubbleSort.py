53a30391207101d65175bad9ad91674fb8e2f633
veken1199 <super_veko@yahoo.com>
2018-01-04 00:42:46 -0500

    bubble sort implementation

---
+++ Python/BubbleSort.py
@@ -0,0 +1,14 @@
+def BubbleSort(arr):
+      dirty = True
+      
+      while(dirty):
+            dirty = False
+            for i in range(len(arr)-1):
+                  if(arr[i] > arr[i+1]):
+                        arr[i],arr[i+1] = arr[i+1], arr[i]
+                        dirty = True
+
+      return arr
+
+print(BubbleSort([2,3,1,9,6,4,2,3,1,55,2,33,4,12,-1,2,-120,3]))
+
