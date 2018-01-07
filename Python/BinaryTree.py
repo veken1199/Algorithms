2750c09793b24508ee9a29a16055996d7f2020b1
veken1199 <super_veko@yahoo.com>
2018-01-07 17:49:26 -0500

    working on binary sort

---
+++ Python/BinaryTree.py
@@ -0,0 +1,22 @@
+class BinaryNode:
+      def __init__(self, val):
+            self.val   = val
+            self.right = None
+            self.left  = None
+
+class BinaryTree:
+      def __init__(self):
+            self.root = None
+      
+      def getRoot(self):
+            self.getRoot
+      
+      def insertNode(self, val):
+            if(self.root == None):
+                  self.root = BinaryNode(val)
+            
+            else:
+                  self.insertNode(val, self.root)
+            
+      def insertNode(self, val, node):
+            if(node.val )
